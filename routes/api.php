c7aeb1c68b3443a05afd5cfe5624a5904480665a
mesh-dell <mwaimonicas@gmail.com>
2026-03-31 03:36:59 +0300

    feat: define API routes for task management with TaskController

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(TaskController::class)->group(function () {
+    Route::get('/tasks/report', 'report');
+    Route::get('/tasks', 'index');
+    Route::post('/tasks', 'store');
+    Route::get('/tasks/{task}', 'show');
+    Route::patch('/tasks/{task}/status', 'update');
+    Route::delete('/tasks/{task}', 'destroy');
+});
